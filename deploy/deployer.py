4df497b0115a489ee3a91a062c852a98e2d07b46
Harsha Narayana <harsha2k4@gmail.com>
2019-09-19 23:44:35 +0530

    :lock: add ssl cert ceneration to python code

--- deploy/deployer.py
+++ deploy/deployer.py
@@ -8,7 +8,11 @@ from base64 import b64encode
 from jinja2 import Environment, BaseLoader, Template
 from os import path, walk
 from yaml import safe_load
+from datetime import datetime
+from time import sleep
 
+from subprocess import check_output
+from shutil import copyfile
 
 import logging
 from pylogrus import PyLogrus, TextFormatter
@@ -31,8 +35,14 @@ config.load_kube_config()
 corev1_api = client.CoreV1Api(client.ApiClient())
 apps_v1_api = client.AppsV1Api(client.ApiClient())
 webhook_api = client.AdmissionregistrationV1beta1Api(client.ApiClient())
+cert_api = client.CertificatesV1beta1Api(client.ApiClient())
+
 
 APP_NAME = "maglev-admission-webhook"
+SECRET = "shhhh!donottell"
+NAMESPACE = "default"
+CSR_NAME = "{}.{}".format(APP_NAME, NAMESPACE)
+
 TEMPLATE_ENV = Environment(loader=BaseLoader)
 RESOURCE_CREATION_MAP = {
     "Deployment": {"handler": apps_v1_api.create_namespaced_deployment},
@@ -72,14 +82,6 @@ def _extract_ca_bundle():
     return b64encode(data.data["client-ca-file"].encode()).decode()
 
 
-def cleanup():
-    _cleanup_mutating_hooks()
-    _cleanup_validating_hooks()
-    _cleanup_service()
-    _cleanup_deployment()
-    _cleanup_config_map()
-
-
 @ignore_404
 def _cleanup_config_map():
     logger.info("Deleting config map for %s", APP_NAME)
@@ -110,6 +112,25 @@ def _cleanup_mutating_hooks():
     webhook_api.delete_mutating_webhook_configuration(name=APP_NAME)
 
 
+@ignore_404
+def _cleanup_csr():
+    logger.info("Clearning up CSR for %s", CSR_NAME)
+    cert_api.delete_certificate_signing_request(name=CSR_NAME)
+
+
+def cleanup():
+    _cleanup_mutating_hooks()
+    _cleanup_validating_hooks()
+    _cleanup_service()
+    _cleanup_deployment()
+    _cleanup_config_map()
+
+
+def _openssl(*args):
+    cmd = ["openssl"] + list(args)
+    return check_output(cmd)
+
+
 def deploy(ca_bundle):
     base_path = path.join(path.dirname(path.abspath(__file__)), "artifacts")
     for root, dirs, files in walk(base_path, topdown=True):
@@ -127,14 +148,107 @@ def deploy(ca_bundle):
                 processor["handler"](body=rendered_data)
 
 
+def build_certs():
+    base_path = path.join(path.dirname(path.abspath(__file__)), "certs")
+    ssl_path = path.join(
+        path.dirname(path.dirname(path.abspath(__file__))),
+        "ssl"
+    )
+    with open(path.join(base_path, "csr-conf.tpl")) as fh:
+        data = fh.read()
+
+    template = TEMPLATE_ENV.from_string(data)
+    rendered_data = template.render(app_name=APP_NAME, namespace=NAMESPACE)
+    with open("/tmp/csr.conf", "w") as fh:
+        fh.write(rendered_data)
+
+    logger.info("Generating RSA Key file...")
+    _ = _openssl("genrsa", "-out", "/tmp/server-key.pem", "2048")
+
+    logger.info("Generating CSR Request data...")
+    _ = _openssl(
+        "req",
+        "-new",
+        "-key",
+        "/tmp/server-key.pem",
+        "-subj",
+        "/CN={}.{}.svc".format(APP_NAME, NAMESPACE),
+        "-out",
+        "/tmp/server.csr",
+        "-config",
+        "/tmp/csr.conf",
+    )
+
+    _cleanup_csr()
+
+    with open("/tmp/server.csr") as fh:
+        request_data = fh.read()
+
+    with open(path.join(base_path, "csr.yaml")) as fh:
+        data = fh.read()
+
+    template = TEMPLATE_ENV.from_string(data)
+    rendered_data = template.render(
+        csr_name=CSR_NAME, csr_data=b64encode(request_data.encode()).decode()
+    )
+    rendered_data = safe_load(rendered_data)
+
+    logger.info("Creating a CSR for %s", CSR_NAME)
+
+    cert_api.create_certificate_signing_request(body=rendered_data)
+
+    logger.info("Approving CSR for %s", CSR_NAME)
+    approval_condition = client.V1beta1CertificateSigningRequestCondition(
+        last_update_time=datetime.now().astimezone(),
+        message="This certificate was approved by Python Client API",
+        reason="MyOwnReason",
+        type="Approved",
+    )
+    body = cert_api.read_certificate_signing_request_status(name=CSR_NAME)
+    body.status.conditions = [approval_condition]
+    cert_api.replace_certificate_signing_request_approval(name=CSR_NAME, body=body)
+
+    for attempt in range(10):
+        logger.info(
+            "[Attempt %d] Checking if the CSR for %s is approved", attempt, CSR_NAME
+        )
+        csr_data = cert_api.read_certificate_signing_request(name=CSR_NAME)
+        logger.info(csr_data.status.certificate)
+        if csr_data.status.certificate:
+            with open("/tmp/cert.data", "w") as fh:
+                fh.write(csr_data.status.certificate)
+
+            _ = _openssl(
+                "base64",
+                "-d",
+                "-A",
+                "-out",
+                "/tmp/server-cert.pem",
+                "-in",
+                "/tmp/cert.data"
+            )
+            break
+        else:
+            sleep(1)
+    else:
+        logger.error("Failed to generate singed certs for %s", CSR_NAME)
+        exit(1)
+
+    logger.info("Successfully completed generating Certs for %s", CSR_NAME)
+    copyfile("/tmp/server-key.pem", path.join(ssl_path, "key.pem"))
+    copyfile("/tmp/server-cert.pem", path.join(ssl_path, "cert.pem"))
+
+
 if __name__ == "__main__":
     parser = ArgumentParser(description="Python Admission controller demo deployer")
     parser.add_argument(
-        "--mode", "-m", choices=("deploy", "cleanup"), default="deploy"
+        "--mode", "-m", choices=("deploy", "cleanup", "certs"), default="deploy"
     )
     args = parser.parse_args()
     if args.mode == "deploy":
         cleanup()
         deploy(_extract_ca_bundle())
-    else:
+    elif args.mode == "cleanup":
         cleanup()
+    else:
+        build_certs()
