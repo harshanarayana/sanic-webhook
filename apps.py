b9046cd93697d406d8308385603f390faf6e7fa8
Harsha Narayana <harshnar@cisco.com>
2022-02-23 21:42:00 +0530

    fix linter issues

--- apps.py
+++ apps.py
@@ -1,14 +1,15 @@
+import base64
+import json as nativejson
 import os
-from sanic import Sanic, Request
-from sanic.response import json
-from sanic.log import logger
 from copy import deepcopy
 
-import json as nativejson
 import jsonpatch
-import base64
+from sanic import Request, Sanic
+from sanic.log import logger
+from sanic.response import json
 
 app = Sanic(name=__name__)
+
 
 @app.post("/audit")
 async def audit(request: Request):
@@ -23,8 +24,8 @@ async def audit(request: Request):
                 "allowed": True,
                 "warnings": [
                     "this is really fun to do but equally annoying to see",
-                ]
-            }
+                ],
+            },
         }
     )
 
@@ -33,8 +34,15 @@ async def audit(request: Request):
 async def disable_host_mounts(request: Request):
     original_request = request.json
     deployment = deepcopy(original_request["request"]["object"])
-    volumes = deployment.get("spec", {}).get("template", {}).get("spec", {}).get("volumes", [])
-    logger.info(f"Processing deployment {deployment.get('name')} for host mount volumes")
+    volumes = (
+        deployment.get("spec", {})
+        .get("template", {})
+        .get("spec", {})
+        .get("volumes", [])
+    )
+    logger.info(
+        f"Processing deployment {deployment.get('name')} for host mount volumes"
+    )
     mounts = []
     new_volumes = []
     for vol in volumes:
@@ -45,22 +53,28 @@ async def disable_host_mounts(request: Request):
 
     if len(volumes) != len(new_volumes):
         deployment["spec"]["template"]["spec"]["volumes"] = new_volumes
-        
+
         for index in range(len(deployment["spec"]["template"]["spec"]["containers"])):
-            volume_mounts = deployment["spec"]["template"]["spec"]["containers"][index].get("volumeMounts")
+            volume_mounts = deployment["spec"]["template"]["spec"]["containers"][
+                index
+            ].get("volumeMounts")
             new_volume_mounts = []
             for vol in volume_mounts:
                 if vol["name"] not in mounts:
                     new_volume_mounts = vol
             if len(volume_mounts) != len(new_volume_mounts):
-                deployment["spec"]["template"]["spec"]["containers"][index]["volumeMounts"] = new_volume_mounts
-    
+                deployment["spec"]["template"]["spec"]["containers"][index][
+                    "volumeMounts"
+                ] = new_volume_mounts
+
     if mounts:
         annotations = deployment["metadata"].get("annotations", {})
         annotations["mks.cisco.com/mutated-for-host-path"] = "true"
         deployment["metadata"]["annotations"] = annotations
 
-        patch = jsonpatch.make_patch(original_request["request"]["object"], deployment).patch
+        patch = jsonpatch.make_patch(
+            original_request["request"]["object"], deployment
+        ).patch
         logger.info(f"JSONPatch {patch}")
         patch = base64.b64encode(nativejson.dumps(patch).encode()).decode()
         oh_no = f"Times have changed, allow we will not, hostMount in Pods. Found violation {','.join(mounts)}"
@@ -73,7 +87,7 @@ async def disable_host_mounts(request: Request):
                     "allowed": True,
                     "warnings": [oh_no],
                     "patchType": "JSONPatch",
-                    "patch": patch
+                    "patch": patch,
                 },
             }
         )
@@ -85,7 +99,7 @@ async def disable_host_mounts(request: Request):
                 "response": {
                     "uid": original_request["request"]["uid"],
                     "allowed": True,
-                }
+                },
             }
         )
 
@@ -94,19 +108,20 @@ async def disable_host_mounts(request: Request):
 async def enforce_resource_requirements(request: Request):
     original_request = request.json
     pod = original_request["request"]["object"]
-    containerMap = {
-        "initContainers": [],
-        "containers": []
-    }
+    containerMap = {"initContainers": [], "containers": []}
     containerMap["initContainers"] = pod["spec"].get("initContainers", [])
     containerMap["containers"] = pod["spec"]["containers"]
     error = False
     for key, containers in containerMap.items():
         logger.info(f"Processing {key} for resource enforcements")
         for container in containers:
-            logger.info(f"Processing container {container['name']} for resource requirements")
+            logger.info(
+                f"Processing container {container['name']} for resource requirements"
+            )
             if container.get("resources") is None:
-                logger.error(f"Container {container['name']} is missing resource information")
+                logger.error(
+                    f"Container {container['name']} is missing resource information"
+                )
                 error = True
 
     if error:
@@ -119,8 +134,8 @@ async def enforce_resource_requirements(request: Request):
                     "allowed": False,
                     "status": {
                         "code": 400,
-                        "message": "I told you this was going to happen!"
-                    }
+                        "message": "I told you this was going to happen!",
+                    },
                 },
             }
         )
@@ -132,9 +147,10 @@ async def enforce_resource_requirements(request: Request):
                 "response": {
                     "uid": original_request["request"]["uid"],
                     "allowed": True,
-                }
+                },
             }
         )
+
 
 ssl = {
     "cert": "/mnt/tls.crt",
@@ -144,4 +160,3 @@ if os.getenv("DEV_MODE"):
     app.run(host="0.0.0.0", port=443)
 else:
     app.run(host="0.0.0.0", port=443, ssl=ssl)
-
